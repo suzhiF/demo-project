1ff47ad1714c9a8b3ece910729ab9c615bf241a2
suzhifa <13963543525@163.com>
2021-05-26 13:30:40 +0800

    打包路径问题

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,6 @@ module.exports = {
     },
     // 浏览器报错 :You are using the runtime-only build of Vue where the template compiler is not available.
     // Either pre-compile the templates into render functions, or use the compiler-included build.
-    runtimeCompiler: true
+    runtimeCompiler: true,
+    publicPath: ''
 }
